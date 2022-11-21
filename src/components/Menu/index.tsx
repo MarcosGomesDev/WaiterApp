bb35ff5b0db3e0423af704c33571f8123456a08b
Marcos Gomes <marcosgomesdev@gmail.com>
2022-11-20 23:34:30 -0300

    formatar moeda & criação do menu componentes

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,10 +1,58 @@
 import React from 'react';
-import { View } from 'react-native';
+import { FlatList, View } from 'react-native';
 
-// import { Container } from './styles';
+import {products} from '../../mocks/products';
+import theme from '../../styles/theme';
+import { FormatCurrency } from '../../utils/FormatCurrency';
+import { PlusCircle } from '../Icons/PlusCircle';
+import { Text } from '../Text';
+
+import {
+    Product,
+    Image,
+    ProductDetails,
+    Separator,
+    AddBtn,
+} from './styles';
 
 const Menu: React.FC = () => {
-    return <View />;
+    return (
+        <FlatList
+            data={products}
+            style={{marginTop: 32}}
+            contentContainerStyle={{paddingHorizontal: 24}}
+            keyExtractor={product => product._id}
+            ItemSeparatorComponent={Separator}
+            renderItem={({item: product}) => (
+                <Product>
+                    <Image source={{
+                        uri: `http://192.168.1.4:3001/uploads/${product.imagePath}`
+                    }} />
+                    <ProductDetails>
+                        <Text
+                            weight={theme.FONTS.GENERAL_SEMI}
+                        >
+                            {product.name}
+                        </Text>
+                        <Text
+                            style={{marginVertical: 8}}
+                            size={14}
+                        >
+                            {product.description}
+                        </Text>
+                        <Text
+                            weight={theme.FONTS.GENERAL_SEMI}
+                        >
+                            {FormatCurrency(product.price)}
+                        </Text>
+                    </ProductDetails>
+                    <AddBtn>
+                        <PlusCircle />
+                    </AddBtn>
+                </Product>
+            )}
+        />
+    );
 }
 
 export default Menu;
