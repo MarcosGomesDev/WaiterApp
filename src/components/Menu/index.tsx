0c4514ea9c42d55bf0a018965be77187cf2e2686
Marcos Gomes <marcosgomesdev@gmail.com>
2022-11-21 01:44:25 -0300

    implementações e criação ProductModal

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList, View } from 'react-native';
+import { Product } from '../../@types/Product';
 
-import {products} from '../../mocks/products';
+import { products } from '../../mocks/products';
 import theme from '../../styles/theme';
 import { FormatCurrency } from '../../utils/FormatCurrency';
 import { PlusCircle } from '../Icons/PlusCircle';
+import ProductModal from '../ProductModal';
 import { Text } from '../Text';
 
 import {
-    Product,
+    ProductContainer,
     Image,
     ProductDetails,
     Separator,
@@ -16,42 +18,60 @@ import {
 } from './styles';
 
 const Menu: React.FC = () => {
+    const [modalVisible, setModalVisible] = useState<boolean>(false);
+    const [selectedProduct, setSelectedProduct] = useState<null | Product>(null)
+
+    const handleOpenModal = (product: Product) => {
+        setModalVisible(!modalVisible);
+        setSelectedProduct(product)
+    }
+
     return (
-        <FlatList
-            data={products}
-            style={{marginTop: 32}}
-            contentContainerStyle={{paddingHorizontal: 24}}
-            keyExtractor={product => product._id}
-            ItemSeparatorComponent={Separator}
-            renderItem={({item: product}) => (
-                <Product>
-                    <Image source={{
-                        uri: `http://192.168.1.4:3001/uploads/${product.imagePath}`
-                    }} />
-                    <ProductDetails>
-                        <Text
-                            weight={theme.FONTS.GENERAL_SEMI}
-                        >
-                            {product.name}
-                        </Text>
-                        <Text
-                            style={{marginVertical: 8}}
-                            size={14}
-                        >
-                            {product.description}
-                        </Text>
-                        <Text
-                            weight={theme.FONTS.GENERAL_SEMI}
-                        >
-                            {FormatCurrency(product.price)}
-                        </Text>
-                    </ProductDetails>
-                    <AddBtn>
-                        <PlusCircle />
-                    </AddBtn>
-                </Product>
-            )}
-        />
+        <>
+            <ProductModal
+                isVisible={modalVisible}
+                onClose={() => setModalVisible(!modalVisible)}
+                product={selectedProduct}
+
+            />
+            <FlatList
+                data={products}
+                style={{ marginTop: 32 }}
+                contentContainerStyle={{ paddingHorizontal: 24 }}
+                keyExtractor={product => product._id}
+                ItemSeparatorComponent={Separator}
+                renderItem={({ item: product }) => (
+                    <ProductContainer
+                        onPress={() => handleOpenModal(product)}
+                    >
+                        <Image source={{
+                            uri: `http://192.168.1.4:3001/uploads/${product.imagePath}`
+                        }} />
+                        <ProductDetails>
+                            <Text
+                                weight={theme.FONTS.GENERAL_SEMI}
+                            >
+                                {product.name}
+                            </Text>
+                            <Text
+                                style={{ marginVertical: 8 }}
+                                size={14}
+                            >
+                                {product.description}
+                            </Text>
+                            <Text
+                                weight={theme.FONTS.GENERAL_SEMI}
+                            >
+                                {FormatCurrency(product.price)}
+                            </Text>
+                        </ProductDetails>
+                        <AddBtn>
+                            <PlusCircle />
+                        </AddBtn>
+                    </ProductContainer>
+                )}
+            />
+        </>
     );
 }
 
---
+++ src/components/ProductModal/index.tsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import { FlatList, Modal, View } from 'react-native';
+import { Product } from '../../@types/Product';
+import theme from '../../styles/theme';
+import { FormatCurrency } from '../../utils/FormatCurrency';
+import Button from '../Button';
+import { Close } from '../Icons/Close';
+import { Text } from '../Text';
+
+import {
+    CloseBtn,
+    Header,
+    Image,
+    ModalBody,
+    IngredientsContainer,
+    Ingredient,
+    Footer,
+    FooterContainer,
+    PriceContainer
+} from './styles';
+
+interface ModalProps {
+    isVisible: boolean;
+    onClose: () => void;
+    product: null | Product;
+}
+
+const ProductModal: React.FC<ModalProps> = ({ isVisible, onClose, product }) => {
+    if (!product) {
+        return null;
+    }
+
+    return (
+        <Modal
+            visible={isVisible}
+            animationType="slide"
+            presentationStyle='pageSheet'
+            onRequestClose={onClose}
+        >
+            <Image
+                source={{
+                    uri: `http://192.168.1.4:3001/uploads/${product.imagePath}`
+                }}
+            >
+                <CloseBtn
+                    onPress={onClose}
+                >
+                    <Close color="#fff" />
+                </CloseBtn>
+            </Image>
+
+            <ModalBody>
+                <Header>
+                    <Text
+                        size={24}
+                        weight={theme.FONTS.GENERAL_SEMI}
+                    >
+                        {product.name}
+                    </Text>
+                    <Text
+                        color="#666"
+                        style={{ marginTop: 8 }}
+                    >
+                        {product.description}
+                    </Text>
+                </Header>
+
+                {product.ingredients.length > 0 && (
+                    <IngredientsContainer>
+                        <Text
+                            weight={theme.FONTS.GENERAL_SEMI}
+                            color="#666"
+                        >
+                            Ingredientes
+                        </Text>
+
+                        <FlatList
+                            data={product.ingredients}
+                            keyExtractor={ingredient => ingredient._id}
+                            style={{
+                                marginTop: 16
+                            }}
+                            showsVerticalScrollIndicator={false}
+                            renderItem={({ item: ingredient }) => (
+                                <Ingredient>
+                                    <Text>{ingredient.icon}</Text>
+                                    <Text
+                                        size={14}
+                                        color="#666"
+                                        style={{ marginLeft: 20 }}
+                                    >
+                                        {ingredient.name}
+                                    </Text>
+                                </Ingredient>
+                            )}
+                        />
+                    </IngredientsContainer>
+                )}
+            </ModalBody>
+
+            <Footer>
+                <FooterContainer>
+                    <PriceContainer>
+                        <Text
+                            color="#666"
+                        >
+                            Preço
+                        </Text>
+                        <Text
+                            size={20}
+                            weight={theme.FONTS.GENERAL_SEMI}
+                        >
+                            {FormatCurrency(product.price)}
+                        </Text>
+                    </PriceContainer>
+                    <Button
+                        onPress={() => { }}
+                    >
+                        Adicionar ao pedido
+                    </Button>
+                </FooterContainer>
+            </Footer>
+        </Modal>
+    );
+}
+
+export default ProductModal;
